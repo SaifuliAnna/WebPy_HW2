339b7e027ca528acc81f12032ac4ea86fb9cbac3
Saifulina Anna <saifulianna.it@gmail.com>
2022-07-16 14:56:11 +0300

    Update cli.py
    new

--- cli.py
+++ cli.py
@@ -1,24 +1,65 @@
-class CliOutputAbstract(metaclass=ABCMeta):
+import abc
+import pickle
 
-    @abstractmethod
+
+class CliOutputAbstract(metaclass=abc.ABCMeta):
+
+    def __init__(self, f_name):
+        self.data = data
+        self.__f_name = None
+        self.f_name = f_name
+
+    @abc.abstractmethod
     def output_cli(self):
         pass
 
 
-class CliOutputInfo(CliOutputAbstract):
+class CliOutputAddBook(CliOutputAbstract):
+
+    @property
+    def f_name(self):
+        return self.__f_name
+
+    @f_name.setter
+    def f_name(self, f_name):
+        parse = f_name.split('.')
+        if parse[1] == 'bin':
+            self.__f_name = f_name
+        else:
+            raise ValueError("File must be BIN format")
 
     def output_cli(self):
-        pass
 
-
-class CliOutputData(CliOutputAbstract):
-
-    def output_cli(self):
-        pass
+        with open(self.__f_name, 'rb') as file:
+            unpacked = pickle.load(file)
+            print(unpacked)
 
 
 class CliOutputNote(CliOutputAbstract):
 
+    @property
+    def f_name(self):
+        return self.__f_name
+
+    @f_name.setter
+    def f_name(self, f_name):
+        parse = f_name.split('.')
+        if parse[1] == 'bin':
+            self.__f_name = f_name
+        else:
+            raise ValueError("File must be BIN format")
+
     def output_cli(self):
-        pass
-    
+
+        with open(self.__f_name, 'rb') as file:
+            unpacked = pickle.load(file)
+            print(unpacked)
+
+
+if __name__ != '__main__':
+
+    bin_add_book = CliOutputAddBook('AddressBook.bin')
+    bin_add_book.output_cli()
+
+    bin_add_note = CliOutputNote('NoteBook.bin')
+    bin_add_note.output_cli()
